d69f3cd575bbdf121765764a68b161674048593c
unknown <Ancelin@.(none)>
2012-04-29 00:03:03 -0400

    Minor style fix.

--- Assets/Scripts/NetworkHelper.cs
+++ Assets/Scripts/NetworkHelper.cs
@@ -3,19 +3,18 @@ using System.Collections;
 using UnityEngine;
 using Sfs2X.Entities.Data;
 
-// This class holds transform values and can load and send the data to server
 public class NetworkHelper {
 	public static ISFSObject TransformToSFSObject(Vector3 pos, Vector3 angles) {
 		ISFSObject data = new SFSObject();
 		
 		data.PutUtfString("type", "transform");
 		data.PutFloat("x", pos.x);
-		data.PutFloat ("y", pos.y);
-		data.PutFloat ("z", pos.z);
+		data.PutFloat("y", pos.y);
+		data.PutFloat("z", pos.z);
 		
 		data.PutFloat("rx", angles.x);
-		data.PutFloat ("ry", angles.y);
-		data.PutFloat ("rz", angles.z);
+		data.PutFloat("ry", angles.y);
+		data.PutFloat("rz", angles.z);
 		return data;
 	}
 	
@@ -23,15 +22,14 @@ public class NetworkHelper {
 		return new SFSObject();
 	}
 	
-	// Creating NetworkTransform from SFS object
 	public static void SFSObjectToTransform(ISFSObject data, Transform trans) {
 		float x = data.GetFloat("x");
-		float y = data.GetFloat ("y");
-		float z = data.GetFloat ("z");
+		float y = data.GetFloat("y");
+		float z = data.GetFloat("z");
 		
-		float rx = data.GetFloat ("rx");
-		float ry = data.GetFloat ("ry");
-		float rz = data.GetFloat ("rz");
+		float rx = data.GetFloat("rx");
+		float ry = data.GetFloat("ry");
+		float rz = data.GetFloat("rz");
 		
 		trans.position = new Vector3(x, y, z);
 		trans.localEulerAngles = new Vector3(rx, ry, rz);
