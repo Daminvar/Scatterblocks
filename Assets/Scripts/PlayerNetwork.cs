964ff87dacf78affb4ae386fdad71c3251b2d906
unknown <wjz4804@OH1375-6.main.ad.rit.edu>
2012-05-01 20:22:34 -0400

    Adds player death/resets.

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -12,6 +12,8 @@ using Sfs2X.Logging;
 public class PlayerNetwork : MonoBehaviour {
 	private SmartFox smartFox;
 	
+	private Vector3 startingTransform;
+	
 	private bool _isBlueTeam;
 	public bool IsBlueTeam {
 		get { return _isBlueTeam; }
@@ -25,12 +27,32 @@ public class PlayerNetwork : MonoBehaviour {
 		}
 	}
 	
+	private int farthestDistance;
+	public int FarthestDistance
+	{
+		get { return farthestDistance; }
+	}
+	
 	// Use this for initialization
 	void Start () {
 		smartFox = SmartFoxConnection.Connection;
+		
+		startingTransform = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if (this.transform.position.y < 7.1f)
+		{
+			Die();	
+		}
+	}
+	
+	void Die()
+	{
+		Debug.Log("AAAAAARRRRRRGH!");
+		
+		this.transform.position = startingTransform;
 	}
 }
