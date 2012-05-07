c2f11830e2bba2f5a8aae54552450f7da68dc8fb
unknown <bsd4861@NM2550-23.main.ad.rit.edu>
2012-05-07 15:53:49 -0400

    Adds charging

--- Assets/Scripts/ClickPlane.cs
+++ Assets/Scripts/ClickPlane.cs
@@ -12,29 +12,79 @@ using Sfs2X.Logging;
 public class ClickPlane : MonoBehaviour {
 	private SmartFox smartFox;
 	
+	private float startHold;
+	private const float MAX_TIME = 3.0f;
+	private const float CHARGE_MULTIPLIER = 40.0f;
+	private Vector3 explosionPos;
+	private Vector2 size = new Vector2(20.0f, 40.0f);
+	private Vector3 mousePos;
+	
+	private bool isCharging = false;
+	private float progress = 0.0f;
+	
+	public Texture2D chargeBarEmpty;
+	public Texture2D chargeBarFull;
+	
 	// Use this for initialization
 	void Start () 
 	{
 		smartFox = SmartFoxConnection.Connection;
+		chargeBarEmpty = (Texture2D)Resources.Load("chargeBackground");
+		chargeBarFull = (Texture2D)Resources.Load("chargeFull");
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate ()
+	void Update ()
 	{
 		RaycastHit hit;
 		if (Input.GetMouseButtonDown (0))
         {
 			//Debug.Log("Click");
+			
+			// Ensures the player clicked within the clickable region
 			if (!Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit, 10000, 1 << 8))
 			{
 				return;
 			}
 			
-      		//our target is where we clicked
-			var pos = hit.point;
+			mousePos = Input.mousePosition;
+			startHold = Time.time;
+			isCharging = true;
+			Screen.showCursor = false;
 			
+      		//our target is where we clicked
+			explosionPos = hit.point;		
 			//Make object message.
-			sendExplosionForce(pos, 100.0f);
+			//sendExplosionForce(pos, 100.0f);
+		}
+		if (Input.GetMouseButtonUp (0))
+		{
+			float totalCharge =  (Time.time - startHold) * CHARGE_MULTIPLIER;
+			
+			if (totalCharge > 120.0f)
+			{
+				totalCharge = 120.0f;	
+			}
+			
+			sendExplosionForce(explosionPos, totalCharge);
+			
+			isCharging = false;
+			
+			Screen.showCursor = true;
+			
+		}
+		if (isCharging)
+		{
+			progress = (Time.time - startHold)/MAX_TIME;
+		}
+	}
+	
+	void OnGUI()
+	{
+		if (isCharging)
+		{
+    		GUI.DrawTexture(new Rect(mousePos.x, Screen.height - mousePos.y, size.x, -size.y * Mathf.Clamp01(progress)), chargeBarFull, ScaleMode.StretchToFill);
+			GUI.DrawTexture(new Rect(mousePos.x, Screen.height - mousePos.y - size.y, size.x, size.y), chargeBarEmpty);
 		}
 	}
 	
